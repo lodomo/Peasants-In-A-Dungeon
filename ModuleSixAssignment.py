e47aa16b79d00b80b2cb86fc651338f9c67e9ea6
LorenzoDMoon <76186542+LorenzoDMoon@users.noreply.github.com>
2021-01-10 00:24:58 -0800

    Add files via upload

--- ModuleSixAssignment.py
+++ ModuleSixAssignment.py
@@ -1,4 +1,5 @@
 # Loren Haskins
+import sys
 import time
 
 # Constants
@@ -18,7 +19,7 @@ MirrorRoom = 'Mirror Room'
 KeyRoom = 'Key Room'
 FrogRoom = 'Frog Room'
 BossRoom = 'Boss Room'
-type_speed = 0.03
+type_speed = 0.025
 
 rooms = {
     Chamber: {East: ChestRoom, West: Shop, North: PrismRoom},
@@ -30,8 +31,7 @@ rooms = {
     SkeletonRoom: {South: RiddleRoom, East: MirrorRoom},
     MirrorRoom: {North: FrogRoom, South: KeyRoom, West: SkeletonRoom},
     KeyRoom: {North: MirrorRoom},
-    FrogRoom: {South: MirrorRoom, Down: BossRoom},
-    BossRoom: {}
+    FrogRoom: {South: MirrorRoom, Down: BossRoom}
 }
 
 
@@ -39,37 +39,51 @@ def wait(fl):
     time.sleep(fl)
 
 
-def type(string):
-    if type_speed != 0:
+def print_type(string):
+    if type_speed == 0:
+        print(string)
+    else:
         string = string.split()
         length = range(len(string))
         for i in length:
             word = list(string[i])
             for char in word:
                 print(char, end='')
+                sys.stdout.flush()
                 wait(type_speed)
             if i != length:
                 print(' ', end='')
                 wait(type_speed)
         print('')
-    else:
-        print(string)
 
 
 '''GAME STARTS HERE'''
-
-type('Welcome to the prologue to Peasants in a Dungeon.')
-type('Before we begin, would you like to turn off this typing effect? Y or N')
+input()
+print_type('Before we begin, would you like to turn off this typing effect?')
 
 while True:
     set_type = input().upper()
     if set_type == 'Y' or set_type == 'YES':
         type_speed = 0
-        type('That\'s okay. It\'s not like I spent a lot of time creating an immersive experience')
+        print_type('That\'s okay. It\'s not like I spent a lot of time creating an immersive experience')
         break
-    elif set_type != 'N' or set_type == 'NO':
-        type('Sorry that is not an option. Try again.')
-        type('Would you like to turn off this typing effect? Y or N')
+    elif set_type == 'N' or set_type == 'NO':
+        print_type('Thank you.')
+        break
     else:
-        type('Thank you.')
-        break
+        print_type('Sorry that is not an option. Try again.')
+        print_type('Would you like to turn off this typing effect? Y or N')
+
+print()
+print_type('Welcome to the prologue to Peasants in a Dungeon.')
+print()
+print_type('You find yourself on a bumpy road in the middle of nowhere. Two paladins are taking you farther away from')
+print_type('your home than you have ever been before, but how often do you get offered a job from the king?!')
+print_type('The Mad King Miller offered you more money than you have made in your entire gnome life')
+print()
+print_type('How hard could this be?!')
+print()
+print_type(' One afternoon\'s work and you will be able to surprise your wife with a farm and an honest life')
+print_type('')
+
+input()
